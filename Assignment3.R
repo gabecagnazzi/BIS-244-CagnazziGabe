528f7200fb35a17b3eff27a91cf63526165af65e
Gabe Cagnazzi <gjc222@lehigh.edu>
2020-10-01 17:35:36 -0600

    Assignment 3

---
+++ Assignment3.R
@@ -0,0 +1,19 @@
+library(dplyr)
+library(readr)
+library(tidyverse)
+
+us_counties.csv <- read_csv("covid-19-data/us-counties.csv")
+
+
+us_lehigh_county <- us_counties.csv %>% filter(county=="Lehigh")
+
+p <- ggplot() + 
+  geom_point(data = us_lehigh_county, mapping = aes(x = date, y = deaths, color = "Deaths")) +
+  geom_point(data = us_lehigh_county, mapping = aes(x = date, y = cases, color = "Cases")) +
+  labs(x = "Date", y = "COVID-19 Cases and Deaths",
+       title = "COVID-19 Cases and Deaths in Lehigh County",
+       subtitle = "Data points are cumulative cases",
+       caption = "Source: NY Times web GitGub repo", 
+       colour = "Cases or Deaths")
+
+p
